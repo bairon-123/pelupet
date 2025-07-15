dd41981f08f44e108213b5f6a44e2c16ba502c7f
bairon-123 <bairon.andres.ga@gmail.com>
2025-07-15 00:10:21 -0400

    cambios para la app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,15 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'SkeletonAPP',
-  webDir: 'www',
-
-plugins: {
-  Geolocation: {
-    enabled: true
-  }
-}
+  appId: 'com.pelupet.app',   // Cambia por el identificador que uses
+  appName: 'PeluPet',
+  webDir: 'www'
 
 };
 
